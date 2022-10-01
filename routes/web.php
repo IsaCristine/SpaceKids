ea9ea87921bed5d8e9f2bbbcf6f0f427bf92eab6
IsaCristine <fredericodgv@gmail.com>
2022-09-30 21:07:48 -0300

    Cria exemplo de rotas

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+// Exemplo de rota simples:
+
+// Route::get('/caminho_url', function () {
+//     return view('caminho_da_view'); //dentro da pasta resources/views
+// });
+
 
 Route::get('/', function () {
     return view('welcome');
