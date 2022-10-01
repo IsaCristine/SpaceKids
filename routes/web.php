1d24ca94e20ca9d0c1874682efbbbfcf94cd6337
IsaCristine <fredericodgv@gmail.com>
2022-09-30 23:23:49 -0300

    Adiciona o bootstrap

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,9 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('head');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
