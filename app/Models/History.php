c540017e124fd67e774a1dc2a2bfafd2d874077a
Frederico Dôndici Gama Vieira <fredericodgv@gmail.com>
2022-12-04 20:14:03 -0300

    Cria arquivos de Model e Controller de History

---
+++ app/Models/History.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class History extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'image',
+        'paragraph1',
+        'paragraph2',
+        'paragraph3',
+        'origin'
+    ];
+}
