6ab615a6ed5f265ca0e161037189aff4a13e23d7
IsaCristine <bell2011jf@gmail.com>
2022-12-21 18:53:26 -0300

    Cria arquivo de histórias

---
+++ resources/views/site/histories.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{--Importa todo o Bootstrap:--}}
+    @vite(['resources/js/app.js'])
+    {{--Importa o CSS utilizado na rota:--}}
+    <link rel="stylesheet" href="/css/{{$css}}">	
+
+    <title>Histórias</title>
+</head>
+
+
+
+<body>
+    {{--Seu código aqui:--}}
+</body>
+
+</html>
