3e5db7329e57c7190f95bdc4a5db4814573f0c13
IsaCristine <bell2011jf@gmail.com>
2022-12-22 00:00:41 -0300

    Insere novos cards para teste

--- resources/views/site/histories.blade.php
+++ resources/views/site/histories.blade.php
@@ -16,30 +16,110 @@
 
 
 <body>
+    {{--Container geral da página:--}}
     <div class="container histories-container">
 
+        {{--Título da seção:--}}
         <div class="theme-title">
             <p>Histórias sobre o Tema</p>
         </div>
 
+        {{--Barra de pesquisa:--}}
         <div class="search-bar">
             {{--Search Bar--}}
         </div>
 
+        {{--Cards de histórias:--}}
         <div class="histories-table">
-            {{--Histories--}}
             <div class="row d-flex justify-content-center">
+                {{--Cards de uma row:--}}
                 <div class="cards-row">
+
+                    {{--Card 1:--}}
                     <div class="col">
-                        <div class="card" style="width: 15rem; height: 18rem">
+                        <div class="card card-style" style="width: 15rem; height: 18rem">
                             <div class="card-body">
                               <div class="btn-card">
-                                <div class="btn btn-primary disabled history-btn title-btn"><b>Título</b></div>
+                                <div class="btn btn-primary disabled title-btn"><b>Título</b></div>
                                 <div class="btn btn-primary history-btn write-btn">Ler</div>
                               </div>
                             </div>
-                          </div>
+                        </div>
                     </div>
+                    {{--Fim do Card 1--}}
+
+                    {{--Card 2:--}}
+                    <div class="col">
+                        <div class="card card-style" style="width: 15rem; height: 18rem">
+                            <div class="card-body">
+                              <div class="btn-card">
+                                <div class="btn btn-primary disabled title-btn"><b>Título</b></div>
+                                <div class="btn btn-primary history-btn write-btn">Ler</div>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {{--Card 3:--}}
+                    <div class="col">
+                        <div class="card card-style" style="width: 15rem; height: 18rem">
+                            <div class="card-body">
+                              <div class="btn-card">
+                                <div class="btn btn-primary disabled title-btn"><b>Título</b></div>
+                                <div class="btn btn-primary history-btn write-btn">Ler</div>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {{--Card 4:--}}
+                    <div class="col">
+                        <div class="card card-style" style="width: 15rem; height: 18rem">
+                            <div class="card-body">
+                              <div class="btn-card">
+                                <div class="btn btn-primary disabled title-btn"><b>Título</b></div>
+                                <div class="btn btn-primary history-btn write-btn">Ler</div>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {{--Card 5:--}}
+                    <div class="col">
+                        <div class="card card-style" style="width: 15rem; height: 18rem">
+                            <div class="card-body">
+                              <div class="btn-card">
+                                <div class="btn btn-primary disabled title-btn"><b>Título</b></div>
+                                <div class="btn btn-primary history-btn write-btn">Ler</div>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {{--Card 6:--}}
+                    <div class="col">
+                        <div class="card card-style" style="width: 15rem; height: 18rem">
+                            <div class="card-body">
+                              <div class="btn-card">
+                                <div class="btn btn-primary disabled title-btn"><b>Título</b></div>
+                                <div class="btn btn-primary history-btn write-btn">Ler</div>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {{--Card 7:--}}
+                    <div class="col">
+                        <div class="card card-style" style="width: 15rem; height: 18rem">
+                            <div class="card-body">
+                              <div class="btn-card">
+                                <div class="btn btn-primary disabled title-btn"><b>Título</b></div>
+                                <div class="btn btn-primary history-btn write-btn">Ler</div>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
