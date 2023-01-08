4698c6add884649aebdec51197a2d5c9bcb02bb0
Daniel <fagundes.daniel191@gmail.com>
2023-01-08 01:48:12 -0300

    pagina adm, navbar

--- resources/views/site/adm.blade.php
+++ resources/views/site/adm.blade.php
@@ -7,23 +7,54 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{--Importa todo o Bootstrap:--}}
     @vite(['resources/js/app.js'])
+    @vite(['resources/js/nav.js'])
+
     {{--Importa o CSS utilizado na rota:--}}
     <link rel="stylesheet" href="/css/{{$css}}">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <title>Historias</title>
 </head>
 
 
 <body>
 
+<button class="nav-toggle" aria-controls="primary-navigation" aria-expanded="false">
+    <i class="fas fa-bars"></i>
+</button>
+
+
+{{-- navbar --}}
+<nav>
+{{--    <div class="logo-container" data-nav-visible="false">--}}
+{{--        <img src="assets/logo.png" class="logo" alt="logo">--}}
+{{--    </div>--}}
+    <ul id="primary-navigation" data-visible="false" class="primary-navigation flex">
+        <li class="active">
+            <a href="#" class="btn btn--entrar">Espaco</a>
+        </li>
+        <li class="active">
+            <a href="#" class="btn btn--entrar">Terra</a>
+        </li>
+        <li class="active">
+            <a href="#" class="btn btn--entrar">Fantasia</a>
+        </li>
+        <li class="active">
+            <a href="#" class="btn btn--entrar">Administradores</a>
+        </li>
+        <li class="active">
+            <a href="#" class="btn btn--logout">Sair</a>
+        </li>
+    </ul>
+</nav>
+
+
 {{--Container geral da página:--}}
-<div class="container login-container">
+<div class="container adm-container" data-nav-visible="false">
 
     {{--Título da seção:--}}
     <div class="theme-title">
         <p>Bem-vindo(a), administrador!</p>
     </div>
-
 
 
     {{-- page with cards arranged in a grid, each card should contain a picture on top, follow by a title and a button to access--}}
@@ -98,10 +129,6 @@
 
 
 </div>
-
-
-</div>
-
 
 </body>
 
