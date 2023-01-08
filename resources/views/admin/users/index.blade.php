1aabf8ffe0a615bb2d173d8a8f687e17cec13ad7
IsaCristine <bell2011jf@gmail.com>
2023-01-08 16:09:30 -0300

    Ajusta posições dos botões da tabela

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -14,63 +14,84 @@
 </head>
 
 <body>
-    <header>
-        <h1 class="title">Administradores</h1>
-    </header>
-
-    <!-- Button trigger modal -->
-    <div class="container create-btn">
-        <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#createAdmin">
-            Adicionar <i class="bi bi-plus-circle"></i>
-        </button>
-    </div>
-
-    @include('admin.users.create')
-
     <div class="container">
-        <table class="table table-striped users-table">
-            <thead>
-                <tr>
-                    <th scope="col">Nome do Administrador</th>
-                    <th scope="col"></th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($users as $user)
-                    <tr>
-                        <th scope="row">{{ $user->name }}</th>
-                        <td>
-                            <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#showAdmin{{ $user->id }}"><i
-                                    class="bi bi-eye"></i></button>
-                            @include('admin.users.show')
-                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editAdmin{{ $user->id }}"><i
-                                    class="bi bi-pencil-square"></i></button>
-                            @include('admin.users.edit')
-                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteAdmin{{ $user->id }}"><i
-                                    class="bi bi-trash3"></i></button>
-                            @include('admin.users.delete')
-                        </td>
-                    </tr>
-                @endforeach
+        
+        <div class="top-page row">
+            <header class="col">
+                <h1 class="title">Administradores</h1>
+            </header>
 
-            </tbody>
-        </table>
-
-        <div class="pagination">
-            <nav aria-label="Page navigation example">
-                <ul class="pagination">
-                  <li class="page-item disabled">
-                    <a class="page-link">Voltar</a>
-                  </li>
-                  <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                  <li class="page-item"><a class="page-link" href="#">2</a></li>
-                  <li class="page-item">
-                    <a class="page-link" href="#">Avançar</a>
-                  </li>
-                </ul>
-              </nav>
+            <div class="col search-bar">
+                <form class="d-flex">
+                    <input class="form-control me-2 search" type="search" placeholder="Pesquisar" aria-label="Search">
+                    <button class="btn btn-secondary btn-search" type="submit">
+                        <i class="bi bi-search"></i>
+                    </button>
+                </form>
+            </div>
+        
+            <!-- Button trigger modal -->
+            <div class="col container create-btn">
+                <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#createAdmin">
+                    Adicionar <i class="bi bi-plus-circle"></i>
+                </button>
+            </div>
         </div>
-
+    
+        @include('admin.users.create')
+    
+        <div class="container">
+            <table class="table table-striped users-table">
+                <thead>
+                    <tr>
+                        <th scope="col">Nome do Administrador</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($users as $user)
+                        <tr>
+                            <th scope="row">{{ $user->name }}</th>
+                            <td>
+                                <div class="all-btn-table">
+                                    <button type="button" class="btn btn-secondary btn-table" data-bs-toggle="modal" data-bs-target="#showAdmin{{ $user->id }}"><i
+                                        class="bi bi-eye"></i></button>
+                                    @include('admin.users.show')
+                                    <button type="button" class="btn btn-primary btn-table" data-bs-toggle="modal" data-bs-target="#editAdmin{{ $user->id }}"><i
+                                        class="bi bi-pencil-square"></i></button>
+                                    @include('admin.users.edit')
+                                    <button type="button" class="btn btn-danger btn-table" data-bs-toggle="modal" data-bs-target="#deleteAdmin{{ $user->id }}"><i
+                                        class="bi bi-trash3"></i></button>
+                                    @include('admin.users.delete')
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+    
+                </tbody>
+            </table>
+    
+            <div class="pagination">
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination">
+                        <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Previous">
+                              <span aria-hidden="true">&laquo;</span>
+                            </a>
+                          </li>
+                          <li class="page-item"><a class="page-link" href="#">1</a></li>
+                          <li class="page-item"><a class="page-link" href="#">2</a></li>
+                          <li class="page-item"><a class="page-link" href="#">3</a></li>
+                          <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Next">
+                              <span aria-hidden="true">&raquo;</span>
+                            </a>
+                          </li>
+                    </ul>
+                  </nav>
+            </div>
+    
+        </div>
     </div>
 </body>
 
