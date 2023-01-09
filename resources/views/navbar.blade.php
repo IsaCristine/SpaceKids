5a16042e1fb36c251fc0decd671abe52be00e9ab
IsaCristine <bell2011jf@gmail.com>
2023-01-09 20:13:07 -0300

    Atualiza navbar responsiva

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,4 +1,38 @@
-{{-- <nav class="nav">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top nav-site">
+    <div class="container-fluid navbar_site">
+      <a class="navbar-brand nav_logo" href="#">
+        <img src="assets/logo.png" alt=""/>
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon icon-navbar"></span>
+      </button>
+      <div class="collapse navbar-collapse links-navbar" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Início</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('space') }}">Espaço</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('earth') }}">Terra</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('fantasy') }}">Fantasia</a>
+          </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contato</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">Login</a>
+            </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+  {{-- <nav class="nav">
     <div class="container-fluid">
         <a class="nav__logo" href="#">
             <img src="assets/logo.png" alt=""/> </a>
@@ -24,35 +58,3 @@
         </ul>
     </div>
 </nav> --}}
-<nav class="navbar navbar-expand-lg bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand nav_logo" href="#">
-        <img src="assets/logo.png" alt=""/>
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="#">Início</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('space') }}>Espaço</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('earth') }}">Terra</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{ route('fantasy') }}">Fantasia</a>
-          </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Contato</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('login') }}">Login</a>
-            </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
