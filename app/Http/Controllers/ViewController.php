5b5ac56fbf90e5d0b01ff58d74e1f0d73611ac87
Frederico Dôndici Gama Vieira <fredericodgv@gmail.com>
2023-01-09 23:11:10 -0300

    a

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -27,9 +27,9 @@ class ViewController extends Controller
         return view('site.login')->with('css', 'site/login.css');
     }
 
-    public function adm()
+    public function dashboard()
     {
-        return view('site.adm')->with('css', 'site/adm.css');
+        return view('admin.dashboard')->with('css', 'admin/dashboard.css');
     }
 
     public function spaceIndex()
