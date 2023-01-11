4a1e6726eb6e6d3c41b97bfe2602d350c2520f43
IsaCristine <bell2011jf@gmail.com>
2023-01-10 22:27:17 -0300

    Adiciona imagens de exemplo em historyFactory

--- database/factories/HistoryFactory.php
+++ database/factories/HistoryFactory.php
@@ -22,7 +22,7 @@ class HistoryFactory extends Factory
             'paragraph1' => $this->faker->paragraph,
             'paragraph2' => $this->faker->paragraph,
             'paragraph3' => $this->faker->paragraph,
-            'image' => $this->faker->randomElement(['logo.png', 'colored_planet.gif', 'home_bg.gif']),
+            'image' => $this->faker->randomElement(['fantasy_1.gif', 'space_1.gif', 'earth_1.gif', 'fantasy_2.gif', 'space_2.gif', 'earth_2.gif']),
             'origin' => $this->faker->sentence,
         ];
     }
