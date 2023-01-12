fa1ca15e8073b254678c303812f992b854fbf474
IsaCristine <bell2011jf@gmail.com>
2023-01-12 00:34:02 -0300

    Atualiza títulos dos forms dos modais

--- resources/views/admin/history/delete.blade.php
+++ resources/views/admin/history/delete.blade.php
@@ -4,7 +4,7 @@
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Deletar Administrador</h1>
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Deletar História</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <form class="form-delete d-inline-block" action="{{ route('history.delete', $history->id) }}" method="post">
@@ -16,7 +16,7 @@
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                        <button type="submit" class="btn btn-danger">Excluir Usuário</button>
+                        <button type="submit" class="btn btn-danger">Excluir História</button>
                     </div>
                 </form>
             </div>
